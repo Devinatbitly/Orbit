47a312e3f6aa2790d26b71c3c01d6378f151a65e
Devin O. <140081656+Devinatbitly@users.noreply.github.com>
2025-06-24 14:53:09 -0400

    Update orbit_puzzles.js

--- orbit_puzzles.js
+++ orbit_puzzles.js
@@ -2,26 +2,21 @@
 // Format: { left: 'WORD1', right: 'WORD2', answer: 'SOLUTION', leftMissing: number, rightMissing: number }
 
 const ORBIT_PUZZLES = [
-    { left: 'GHOST', right: 'ARTSY', answer: 'START', leftMissing: 2, rightMissing: 3 },
-    { left: 'GREAT', right: 'ENDED', answer: 'EATEN', leftMissing: 3, rightMissing: 2 },
-    { left: 'NACHO', right: 'SERVE', answer: 'CHOSE', leftMissing: 3, rightMissing: 2 },
-    { left: 'GOLEM', right: 'URGED', answer: 'LEMUR', leftMissing: 3, rightMissing: 2 },
-    { left: 'DEALT', right: 'ERASE', answer: 'ALTER', leftMissing: 3, rightMissing: 2 },
-    { left: 'STARE', right: 'ACHED', answer: 'REACH', leftMissing: 2, rightMissing: 3 },
-    { left: 'AHEAD', right: 'OPTED', answer: 'ADOPT', leftMissing: 2, rightMissing: 3 },
-    { left: 'KNEAD', right: 'ULTRA', answer: 'ADULT', leftMissing: 2, rightMissing: 3 },
-    { left: 'DECAF', right: 'TERSE', answer: 'AFTER', leftMissing: 2, rightMissing: 3 },
-    { left: 'EQUAL', right: 'BUMPY', answer: 'ALBUM', leftMissing: 2, rightMissing: 3 },
-    { left: 'FINAL', right: 'LOWLY', answer: 'ALLOW', leftMissing: 2, rightMissing: 3 },
-    { left: 'BEACH', right: 'KINGS', answer: 'CHING', leftMissing: 2, rightMissing: 3 },
-    { left: 'CLOCK', right: 'EARLY', answer: 'CLEAR', leftMissing: 2, rightMissing: 3 },
-    { left: 'PRIDE', right: 'EVENS', answer: 'RAVEN', leftMissing: 2, rightMissing: 3 },
-    { left: 'SLEEP', right: 'FORTH', answer: 'PROOF', leftMissing: 2, rightMissing: 3 },
-    { left: 'TRADE', right: 'OWNER', answer: 'DRONE', leftMissing: 2, rightMissing: 3 },
-    { left: 'SHARK', right: 'PLAID', answer: 'RAPID', leftMissing: 2, rightMissing: 3 },
-    { left: 'GRAIN', right: 'DEPTH', answer: 'AIDED', leftMissing: 2, rightMissing: 3 },
-    { left: 'FLAME', right: 'SPORT', answer: 'ABORT', leftMissing: 2, rightMissing: 3 },
-    { left: 'CROWD', right: 'WHILE', answer: 'OWLHE', leftMissing: 2, rightMissing: 3 }
+            { left: 'GHOST', right: 'ARTSY', answer: 'START', leftMissing: 2, rightMissing: 3 },
+            { left: 'GREAT', right: 'ENDED', answer: 'EATEN', leftMissing: 3, rightMissing: 2 },
+            { left: 'NACHO', right: 'SERVE', answer: 'CHOSE', leftMissing: 3, rightMissing: 2 },
+            { left: 'GOLEM', right: 'URGED', answer: 'LEMUR', leftMissing: 3, rightMissing: 2 },
+            { left: 'DEALT', right: 'ERASE', answer: 'ALTER', leftMissing: 3, rightMissing: 2 },
+            { left: 'STARE', right: 'ACHED', answer: 'REACH', leftMissing: 2, rightMissing: 3 },
+            { left: 'COMET', right: 'HINGE', answer: 'THING', leftMissing: 1, rightMissing: 4 },
+            { left: 'AHEAD', right: 'OPTED', answer: 'ADOPT', leftMissing: 2, rightMissing: 3 },
+            { left: 'KNEAD', right: 'ULTRA', answer: 'ADULT', leftMissing: 2, rightMissing: 3 },
+            { left: 'DECAF', right: 'TERSE', answer: 'AFTER', leftMissing: 2, rightMissing: 3 },
+            { left: 'EQUAL', right: 'BUMPY', answer: 'ALBUM', leftMissing: 2, rightMissing: 3 },
+            { left: 'FINAL', right: 'LOWLY', answer: 'ALLOW', leftMissing: 2, rightMissing: 3 },
+            { left: 'SWARM', right: 'ORDER', answer: 'ARMOR', leftMissing: 3, rightMissing: 2 },
+            { left: 'RANGE', right: 'RIGHT', answer: 'ANGER', leftMissing: 4, rightMissing: 1 },
+            { left: 'MARCH', right: 'ARMOR', answer: 'CHARM', leftMissing: 2, rightMissing: 3 },
 ];
 
 // Export for use in other files
